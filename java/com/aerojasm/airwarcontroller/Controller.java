bcadc02bd9b9bcb6a4568063e4611e253b46f7c0
Andres E. Rojas M <anderojasm@hotmail.com>
2017-04-07 16:32:00 -0600

    Clases base Android
    Clases basicas para hacer el control de Android

---
+++ java/com/aerojasm/airwarcontroller/Controller.java
@@ -0,0 +1,15 @@
+package com.aerojasm.airwarcontroller;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Controller extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_controller);
+
+    }
+
+}
